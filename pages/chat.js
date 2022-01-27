92e11bef61c0b2550be7e3242ddd40c920204513
VINICIUS FAVORITO RODRIGUES <vfavorito7@gmail.com>
2022-01-26 21:09:17 -0300

    Add pagina gr8
    Add pagina gr8

---
+++ pages/chat.js
@@ -0,0 +1,6 @@
+export default function PaginaDoChat(){
+    return(
+        <div>Primeira página do chat</div>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json'
 
-
-
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 
 function titulo(props) {
@@ -68,11 +39,14 @@ function titulo(props) {
 
 
 export default function PaginaInicial() {
-    const username = 'ViniciusFavorito';
+   // const username = 'ViniciusFavorito';
+    const [username,setUsername] = React.useState('ViniciusFavorito');
+    const roteamento = useRouter();
+
 
     return (
         <>
-            <GlobalStyle />
+            
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -99,6 +73,11 @@ export default function PaginaInicial() {
                     {/* Formul�rio */}
                     <Box
                         as="form"
+                        onSubmit={function (e){
+                            e.preventDefault()
+                            console.log("Alguem apertou o botao")
+                            roteamento.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -109,7 +88,28 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
 
+                        {/* <input 
+                            type="text"
+                            value={username}
+                            onChange={function (event){
+                                    console.log("O usuário digitou: ", event.target.value)
+                                    // onde está o valor?
+                                    const valor = event.target.value;
+                                    //trocar valor da variável
+                                    setUsername(valor)
+                                }
+                            }
+                            /> */}
                         <TextField
+                            value={username}
+                            onChange={function (event){
+                                    console.log("O usuário digitou: ", event.target.value)
+                                    // onde está o valor?
+                                    const valor = event.target.value;
+                                    //trocar valor da variável
+                                    setUsername(valor)
+                                }
+                            }
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -131,8 +131,35 @@ export default function PaginaInicial() {
                                 mainColorStrong: appConfig.theme.colors.primary[600],
                             }}
                         />
+                        
                     </Box>
-                    {/* Formul�rio */}
+                    {/* Formul�rio padrão */}
+
+                    <box
+                        as="form"
+                        onSubmit={function (e){
+                            e.preventDefault()
+                            console.log("Alguem apertou o botao")
+                            roteamento.push('/gr8');
+                        }}
+                        styleSheet={{
+                            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                            width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                        }}
+                    >
+
+                    <Button
+                            type='submit'
+                            label='Entrar na aba GR8'
+                            fullWidth
+                            buttonColors={{
+                                contrastColor: appConfig.theme.colors.neutrals["000"],
+                                mainColor: appConfig.theme.colors.primary[500],
+                                mainColorLight: appConfig.theme.colors.primary[400],
+                                mainColorStrong: appConfig.theme.colors.primary[600],
+                            }}
+                        />
+                    </box>
 
 
                     {/* Photo Area */}
